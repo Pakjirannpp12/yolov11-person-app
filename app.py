c3c81dee990b82b3ac5a7b634e7713de6ce4327d
Pakjirannpp12 <NnanNPakjira@gmail.com>
2025-09-21 14:22:34 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import numpy as np
+
 st.title("YOLO Image Detection App :)")
+
 # Load YOLO model
 # model = YOLO("runs/detect/train73/weights/best.pt")
-model = YOLO("yolo11n.ptt")
+model = YOLO("yolo11n.pt")
 # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
 
@@ -13,15 +16,15 @@ uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jp
 if uploaded_image is not None:
   # Show original image
   st.image(uploaded_image , caption="Uploaded Image", use_container_width=True)
-
+  
   # Read image and convert to numpy array
   image = Image.open(uploaded_image)
   image_np = np.array(image)
-
+  
   # Run YOLO inference
   st.info("Running YOLO object detection...")
   results = model.predict(image_np , conf=0.4)
-
+  
   # Draw results on image
   result_image = results[0].plot()
   st.image(result_image , caption="YOLO Detection Result", use_container_width=True)
@@ -31,6 +34,10 @@ if uploaded_image is not None:
   boxes = results[0].boxes
   class_ids = boxes.cls.cpu().numpy().astype(int)
   class_names = [model.names[i] for i in class_ids]
+  
   # Count people
   person_count = class_names.count("person")
   st.write(f"Number of people detected: **{person_count}**")
+
+
+
